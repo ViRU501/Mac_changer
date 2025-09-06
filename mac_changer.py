a070cdda7190fe205e20094f2eed06d01a6254d1
root <root@kali.kali>
2025-09-06 08:33:22 -0400

    Advance Mac changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python
 import subprocess
+import optparse
 
-subprocess.call("ifconfig", shell=True)
-subprocess.call("ifconfig eth0  down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:33:44:55:77", shell=True)
-subprocess.call("ifconfig  eth0 up", shell=True)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface",dest="interface",help="interface to change mac address")
+    parser.add_option("-m","--mac",dest="mac",help="mac address to change")
+    (options,args)=parser.parse_args()
+    return options
+def mac_changer(interface,mac):
+    print("changing mac address")
+    subprocess.call(["ifconfig",interface])
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig",interface,"hw" , "ether", mac])
+    subprocess.call(["ifconfig",interface, "up"])
+options=get_arguments()
+mac_changer(options.interface,options.mac)
+
+
