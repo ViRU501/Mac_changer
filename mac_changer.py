0d2c8cc7a0739aeeab882f0097e56de1bd2901b7
kifayat Mir <mirkifayat01@gmail.com>
2025-09-10 03:39:17 -0400

    added few checks

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 import subprocess
 import optparse
 
@@ -10,11 +11,24 @@ def get_arguments():
     return options
 def mac_changer(interface,mac):
     print("changing mac address")
-    subprocess.call(["ifconfig",interface])
+    # subprocess.call(["ifconfig",interface])
     subprocess.call(["ifconfig",interface,"down"])
     subprocess.call(["ifconfig",interface,"hw" , "ether", mac])
     subprocess.call(["ifconfig",interface, "up"])
+def get_current_mac(interface):
+    ifconfig_results = subprocess.check_output(["ifconfig",interface])
+    mac_add_search = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_results))
+    if mac_add_search:
+        return mac_add_search.group(0)
+    else:
+        print("cound not read mac address")
 options=get_arguments()
+cur_mac=get_current_mac(options.interface)  #here value will be the current address
+print(">current mac add is : "+ cur_mac)
 mac_changer(options.interface,options.mac)
-
+cur_mac=get_current_mac(options.interface)  #here value of cur_mac is changed if above func works properly
+if cur_mac == options.mac:
+    print("mac address changed to : "+cur_mac)
+else:
+    print("mac address not changed")
 
